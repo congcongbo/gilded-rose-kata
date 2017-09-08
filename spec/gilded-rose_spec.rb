a26281f5aa0de78540e019159cc243603401fd49
Cong Cong Bo <cong@hippocampus.ai>
2017-09-08 10:40:01 +0100

    tests written for all existing conditions

---
+++ spec/gilded-rose_spec.rb
@@ -0,0 +1,75 @@
+require 'gilded_rose'
+
+describe GildedRose do
+
+  describe '#update_quality' do
+
+    vest = Item.new("+5 Dexterity Vest", 10, 20) #0
+    brie = Item.new("Aged Brie", 2, 5) #1
+    elixir = Item.new("Elixir of the Mongoose", 0, 7) #2
+    sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 0, 50) #3
+    pass1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20) #4
+    pass2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 40) #5
+    pass3 = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 46) #6
+    cake = Item.new("Conjured Mana Cake", 3, 6) #7
+    stock = GildedRose.new([vest, brie, elixir, sulfuras, pass1, pass2, pass3, cake])
+    stock.update_quality
+
+    it "does not change the name" do
+      expect(stock.items[0].name).to eq "+5 Dexterity Vest"
+    end
+
+    it 'quality is never negative' do
+      expect(stock.items[0].quality).to be >=0
+    end
+
+    it 'quality of an item is <= 50' do
+      expect(stock.items[0].quality).to be <=50
+    end
+
+    it 'if legendary, sell_in and quality stay the same' do
+      expect(stock.items[3].quality).to eq 50
+      expect(stock.items[3].sell_in).to eq 0
+    end
+
+    it 'if maturing, quality increases' do
+      expect(stock.items[1].quality).to eq 6
+    end
+
+    it 'for a pass, quality increases' do
+      expect(stock.items[4].quality).to eq 21
+    end
+
+    it 'for a pass whose sell_in <=10, quality increases by 2' do
+      expect(stock.items[5].quality).to eq 42
+    end
+
+    it 'for a pass whose sell_in <=5, quality increases by 3' do
+      expect(stock.items[6].quality).to eq 49
+    end
+
+    it 'if sell_in <= 0, quality degrades twice as fast' do
+      expect(stock.items[2].quality).to eq 5
+    end
+
+    # it 'if conjured, quality decreases by 2' do
+    #   expect(stock.items[7].quality).to eq 4
+    # end
+
+  end
+end
+
+describe Item do
+  subject(:cake) {described_class.new("cake", 3, 6)}
+  it 'has a name, type, sell_in and quality on initialization' do
+    expect(cake.name).to eq "cake"
+    expect(cake.sell_in).to eq 3
+    expect(cake.quality).to eq 6
+  end
+
+  describe '#to_s' do
+    it 'prints the item details' do
+      expect(cake.to_s).to eq "cake, 3, 6"
+    end
+  end
+end
